13faac4439cf91399654bf00836b7831c154cfcc
Tobias.Schraml <tobias.schraml@xpublisher.com>
2025-03-21 14:47:10 +0100

    feat: checkstyle linter for java

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+-- toggle checkstyle-linter for java files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "java",
+  callback = function()
+    vim.api.nvim_set_keymap(
+      "n",
+      "<leader>cL",
+      ":lua toggle_linter()<CR>",
+      { noremap = true, silent = true, desc = "Toggle checkstyle" }
+    )
+  end,
+})
---
+++ lua/plugins/nvim-lint.lua
@@ -0,0 +1,43 @@
+return {
+  "mfussenegger/nvim-lint",
+  event = "LazyFile",
+
+  config = function()
+    local lint = require("lint")
+    local custom_xml_path = vim.fn.expand("$HOME/.config/checkstyle/custom.xml")
+
+    lint.linters.checkstyle.args = { "-c", custom_xml_path }
+    lint.linters_by_ft["java"] = {
+      "checkstyle",
+    }
+
+    local function getDiagnosticsNamespace()
+      for key, val in pairs(vim.diagnostic.get_namespaces()) do
+        if val.name == "checkstyle" then
+          return key
+        end
+      end
+    end
+
+    local lint_enabled = true
+
+    function _G.toggle_linter()
+      if lint_enabled then
+        vim.diagnostic.reset(getDiagnosticsNamespace())
+        print("Checkstyle disabled")
+      else
+        lint.try_lint()
+        print("Checkstyle enabled")
+      end
+      lint_enabled = not lint_enabled
+    end
+
+    vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost", "InsertLeave", "BufEnter" }, {
+      callback = function()
+        if lint_enabled then
+          lint.try_lint()
+        end
+      end,
+    })
+  end,
+}
