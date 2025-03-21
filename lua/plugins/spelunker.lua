393b0f81ae268ff710b3f5e12c0c553eeead3304
Tobj-sudo <tobias.schraml0@gmx.de>
2025-03-21 17:11:45 +0100

    feat: speelunker config

---
+++ lua/plugins/spelunker.lua
@@ -0,0 +1,17 @@
+return {
+  "kamykn/spelunker.vim",
+  dependencies = {
+    "kamykn/popup-menu.nvim",
+    init = function()
+      vim.schedule(function()
+        vim.cmd([[hi PmenuSel ctermfg=135 ctermbg=239 cterm=NONE guifg=#b26eff guibg=#4e4e4e gui=NONE]])
+      end)
+    end,
+  },
+  init = function()
+    vim.opt.spell = false
+  end,
+  config = function()
+    vim.g.spelunker_disable_uri_checking = 1
+  end,
+}
