f90c47eae9cb00099cfe8fb9388966ffc6ddadc8
Tobj-sudo <tobias.schraml0@gmx.de>
2025-04-01 11:56:42 +0200

    feat: diagnostics

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,6 +5,18 @@ return {
       servers = {
         biome = {},
       },
+      diagnostics = {
+        virtual_text = {
+          severity = {
+            max = vim.diagnostic.severity.WARN,
+          },
+        },
+        virtual_lines = {
+          severity = {
+            min = vim.diagnostic.severity.ERROR,
+          },
+        },
+      },
     },
   },
   {
