1a64a377415a1fbaff590eb235c8f422aa3743e8
Tobj-sudo <tobias.schraml0@gmx.de>
2025-04-25 12:44:35 +0200

    feat: added yanky to plugins

---
+++ lua/plugins/yanky.lua
@@ -0,0 +1,8 @@
+return {
+  "gbprod/yanky.nvim",
+  opts = {},
+  keys = {
+    { "<c-p>", "<Plug>(YankyPreviousEntry)", mode = "n", remap = true },
+    { "<c-n>", "<Plug>(YankyNextEntry)", mode = "n", remap = true },
+  },
+}
