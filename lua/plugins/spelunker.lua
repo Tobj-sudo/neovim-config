64197de83b431d71bd40e1cfdb314d4e346468e9
Tobj-sudo <tobias.schraml0@gmx.de>
2025-04-25 12:42:47 +0200

    clean: uncommented config and disabled instead

--- lua/plugins/spelunker.lua
+++ lua/plugins/spelunker.lua
@@ -1,20 +1,18 @@
-return {}
---  *** commented out due to potential virus issus with popup-menu.nvim ***
---
--- return {
---   "kamykn/spelunker.vim",
---   dependencies = {
---     "kamykn/popup-menu.nvim",
---     init = function()
---       vim.schedule(function()
---         vim.cmd([[hi PmenuSel ctermfg=135 ctermbg=239 cterm=NONE guifg=#b26eff guibg=#4e4e4e gui=NONE]])
---       end)
---     end,
---   },
---   init = function()
---     vim.opt.spell = false
---   end,
---   config = function()
---     vim.g.spelunker_disable_uri_checking = 1
---   end,
--- }
+return {
+  "kamykn/spelunker.vim",
+  enabled = false, -- disabled due to potential virus issus with popup-menu.nvim
+  dependencies = {
+    "kamykn/popup-menu.nvim",
+    init = function()
+      vim.schedule(function()
+        vim.cmd([[hi PmenuSel ctermfg=135 ctermbg=239 cterm=NONE guifg=#b26eff guibg=#4e4e4e gui=NONE]])
+      end)
+    end,
+  },
+  init = function()
+    vim.opt.spell = false
+  end,
+  config = function()
+    vim.g.spelunker_disable_uri_checking = 1
+  end,
+}
