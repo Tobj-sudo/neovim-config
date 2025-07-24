6ba9d3b7d2e6b91fef2ed3230015df5df50e7ccd
Tobj-sudo <tobias.schraml0@gmx.de>
2025-07-24 19:07:26 +0200

    clean: organized lsps

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,11 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      javascript = { "biome", lsp_format = "never" },
+    },
+    default_format_opts = {
+      lsp_format = "fallback",
+    },
+  },
+}
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,50 +1,50 @@
-return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        biome = {},
-      },
-      -- diagnostics = {
-      --   virtual_text = {
-      --     severity = {
-      --       max = vim.diagnostic.severity.WARN,
-      --     },
-      --   },
-      --   virtual_lines = {
-      --     severity = {
-      --       min = vim.diagnostic.severity.ERROR,
-      --     },
-      --   },
-      -- },
-    },
-  },
-  {
-    "nvimtools/none-ls.nvim",
-    -- enabled = false,
-    optional = true,
-    opts = function(_, opts)
-      local nls = require("null-ls").builtins
-      opts.sources = vim.tbl_extend("force", opts.sources, { --override lazyvim's default sources
-        -- ts
-        nls.formatting.biome.with({
-          args = {
-            "check",
-            "--write",
-            "--unsafe",
-            "--formatter-enabled=true",
-            "--organize-imports-enabled=true",
-            "--skip-errors",
-            "--stdin-file-path=$FILENAME",
-          },
-        }),
-        -- other
-        nls.formatting.shfmt.with({
-          filetypes = { "sh", "zsh" },
-        }),
-      })
-      opts.debug = false
-      return opts
-    end,
-  },
-}
+return {}
+-- return {
+--   {
+--     "nvimtools/none-ls.nvim",
+--     enabled = false,
+--     optional = true,
+--     opts = function(_, opts)
+--       local nls = require("null-ls").builtins
+--       opts.sources = vim.tbl_extend("force", opts.sources, { --override lazyvim's default sources
+--         nls.formatting.biome.with({
+--           args = {
+--             "check",
+--             "--write",
+--             "--unsafe",
+--             "--formatter-enabled=true",
+--             "--organize-imports-enabled=true",
+--             "--skip-errors",
+--             "--stdin-file-path=$FILENAME",
+--           },
+--         }),
+--         nls.formatting.shfmt.with({
+--           filetypes = { "sh", "zsh" },
+--         }),
+--       })
+--       opts.debug = false
+--       return opts
+--     end,
+--   },
+--   {
+--     "neovim/nvim-lspconfig",
+--     opts = {
+--       servers = {
+--         biome = {},
+--         vtsls = {},
+--       },
+--       -- diagnostics = {
+--       --   virtual_text = {
+--       --     severity = {
+--       --       max = vim.diagnostic.severity.WARN,
+--       --     },
+--       --   },
+--       --   virtual_lines = {
+--       --     severity = {
+--       --       min = vim.diagnostic.severity.ERROR,
+--       --     },
+--       --   },
+--       -- },
+--     },
+--   },
+-- }
