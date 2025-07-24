2e4d6537f657a8bea2a3f4467f6ecd96ece537ee
Tobj-sudo <tobias.schraml0@gmx.de>
2025-07-24 19:03:51 +0200

    feat: added compiler

---
+++ lua/plugins/compiler.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "Zeioth/compiler.nvim",
+    cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
+    dependencies = { "stevearc/overseer.nvim", "nvim-telescope/telescope.nvim" },
+    opts = {},
+  },
+  {
+    "stevearc/overseer.nvim",
+    commit = "6271cab7ccc4ca840faa93f54440ffae3a3918bd",
+    cmd = { "CompilerOpen", "CompilerToggleResults", "CompilerRedo" },
+    opts = {
+      task_list = {
+        default_detail = 1,
+        max_width = { 100, 0.2 },
+        min_width = { 40, 0.1 },
+        width = nil,
+        max_height = { 20, 0.1 },
+        min_height = 8,
+        height = nil,
+        direction = "right",
+      },
+    },
+  },
+}
