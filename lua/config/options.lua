7edbed88be7d69224bbee0ef376844664e728d00
Tobj-sudo <tobias.schraml0@gmx.de>
2025-07-28 15:35:56 +0200

    feat: osc52 clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,4 +2,26 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.clipboard = "unnamedplus"
+-- vim.g.clipboard = "osc52"
+--
+
+local function no_paste()
+  return {
+    vim.fn.split(vim.fn.getreg(""), "\n"),
+    vim.fn.getregtype(""),
+  }
+end
+
+vim.g.clipboard = {
+  name = "osc52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = no_paste,
+    ["*"] = no_paste,
+  },
+}
+
+vim.o.clipboard = "unnamedplus"
